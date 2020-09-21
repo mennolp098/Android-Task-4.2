1b2b283a8c17fa31a692b4373a6b3c2a0b72a04b
Menno <run_jongejan@hotmail.com>
2020-09-21 19:57:36 +0200

    Game is now working

--- app/src/main/java/com/example/android_task_4_2/GameFragment.kt
+++ app/src/main/java/com/example/android_task_4_2/GameFragment.kt
@@ -6,7 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.item_game_history.*
+import kotlin.random.Random
+
+const val ROCK = 0
+const val PAPER = 1
+const val SCISSORS = 2
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -24,8 +32,42 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }*/
+        view.findViewById<ImageView>(R.id.iv_paper).setOnClickListener {
+            onOptionSelected(view, PAPER)
+        }
+        view.findViewById<ImageView>(R.id.iv_rock).setOnClickListener {
+            onOptionSelected(view, ROCK)
+        }
+        view.findViewById<ImageView>(R.id.iv_scissors).setOnClickListener {
+            onOptionSelected(view, SCISSORS)
+        }
+    }
+
+    private fun onOptionSelected(view: View, option:Int) {
+        val computer = Random.nextInt(0, 3)
+        val computerImageView = view.findViewById<ImageView>(R.id.iv_pc)
+        val youImageView = view.findViewById<ImageView>(R.id.iv_you)
+        val resultTextView = view.findViewById<TextView>(R.id.tv_output)
+
+        when(computer) {
+            0 -> computerImageView.setImageResource(R.drawable.rock)
+            1 -> computerImageView.setImageResource(R.drawable.paper)
+            2 -> computerImageView.setImageResource(R.drawable.scissors)
+        }
+
+        when(option) {
+            0 -> youImageView.setImageResource(R.drawable.rock)
+            1 -> youImageView.setImageResource(R.drawable.paper)
+            2 -> youImageView.setImageResource(R.drawable.scissors)
+        }
+
+        if(option == computer)
+        {
+            resultTextView.text = getString(R.string.draw)
+        } else if((option == ROCK && computer == SCISSORS) || (option == PAPER && computer == ROCK) || (option == SCISSORS && computer == PAPER)) {
+            resultTextView.text = getString(R.string.you_win)
+        } else {
+            resultTextView.text = getString(R.string.you_lose)
+        }
     }
 }
